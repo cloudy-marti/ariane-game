8d61a7135c13bb8e15db63e6ac1cd4d10fe871f2
Cloudy <cloud.kira@gmail.com>
2020-03-10 23:17:34 +0100

    Labyrinth display done

--- projet/Labyrinth.py
+++ projet/Labyrinth.py
@@ -1,4 +1,8 @@
 class Labyrinth :
-	def __init__(self, size, lst) :
-		self.size = size
+	def __init__(self, cell_number, lst) :
+		self.cell_number = cell_number
 		self.lst = lst
+		self.window_size = 800
+		self.window_actual_size = 810
+		self.cell_size = self.window_size/cell_number
+		self.margin = 5
--- projet/parse_map.py
+++ projet/parse_map.py
@@ -20,8 +20,7 @@ def parse_map(pathToFile) :
 	labyrinth = Labyrinth(size, labyrinthMap)
 	return labyrinth
 
-lab = parse_map("maps/labyrinthe1.txt")
-# print(lab.lst)
+lab = parse_map("maps/big/big1.txt")
 
 def display_map(labyrinth) :
 	for row in labyrinth :
@@ -34,69 +33,78 @@ def display_map(labyrinth) :
 
 def draw_map(labyrinth, width, height) :
 
-	margin = 5
-
-	cell_number = labyrinth.size
-	cell_size = width / cell_number
+	margin = labyrinth.margin
+	cell_number = labyrinth.cell_number
+	cell_size = labyrinth.cell_size
 
 	cree_fenetre(width+margin, height+margin)
-	rectangle(margin, margin, width, height, epaisseur=2)
+	# rectangle(margin, margin, width, height, epaisseur=2)
 
-	for i, row in enumerate(labyrinth.lst) :
-		for j, char in enumerate(row) :
-			x = (i/2*width/cell_number) + margin
-			y = (j/2*height/cell_number) + margin
-			if char == '|' :
-				ligne(x, y, x, y + cell_size, epaisseur=2)
-			# if char == '-' :
-			# 	ligne(x, y, x + cell_size, y, epaisseur=2)
-			if char == '+' :
-				point(x, y, epaisseur=5)
-				if(row[i+1] == '-') :
-					ligne(x, y, x + cell_size, y, epaisseur=2)
-				if(labyrinth.lst[i][j+1] == '|') :
-					ligne(x + cell_size, y, x + cell_size, y + cell_size, epaisseur=2)
+	for row, line in enumerate(labyrinth.lst) :
+		for column, char in enumerate(line) :
+			functionDictionnary[char] (labyrinth, row, column)
 
 	attente_clic()
 	ferme_fenetre()
 
+def apply_graphical_offset(labyrinth, position) :
+	return (position / 2 * labyrinth.window_size / labyrinth.cell_number) + labyrinth.margin
 
-# def define_assets(labyrinth) :
-# 	minotaur = []
-# 	wall = []
-# 	for row,line in enumerate(labyrinth) :
-# 		for column,char in enumerate(line) :
-# 			#print("row = {0} and column = {1}".format(row, column))
-# 			if char == 'A' :
-# 				# Ariane
-# 				ariane = Ariane(row, column)
-# 			elif char == 'T' :
-# 				# Thésée
-# 				thesee = Thesee(row, column)
-# 			elif char == 'V' :
-# 				# vertical minotaur
-# 				minotaur.append('v', row, column)
-# 			elif char == 'H' :
-# 				# horizontal minotaur
-# 				minotaur.append('h', row, column)
-# 			elif char == '|' :
-# 				# vertical wall
-# 				wall.append(row, column)
-# 			elif char == '-' :
-# 				# horizontal wall
-# 				wall.append(row, column)
-# 			elif char == "+" :
-# 				# intersection
-# 				wall.append(row, column)
-# 			elif char == 'P' :
-# 				end = EndGame(row, column)
-# 				# door
-# 			else :
-# 				print("bye")
-# 				# do nothing
+def draw_vertical_wall(labyrinth, row, column) :
+	start_point = []
+	start_point.append(apply_graphical_offset(labyrinth, column + 1) - labyrinth.cell_size/2)
+	start_point.append(apply_graphical_offset(labyrinth, row - 1))
+
+	end_point = []
+	end_point.append(apply_graphical_offset(labyrinth, column + 1) - labyrinth.cell_size/2)
+	end_point.append(apply_graphical_offset(labyrinth, row + 1))
+
+	ligne(start_point[0], start_point[1], end_point[0], end_point[1], epaisseur=2)
+
+def draw_horizontal_wall(labyrinth, row, column) :
+	start_point = []
+	start_point.append(apply_graphical_offset(labyrinth, column - 1))
+	start_point.append(apply_graphical_offset(labyrinth, row + 1) - labyrinth.cell_size/2)
+
+	end_point = []
+	end_point.append(apply_graphical_offset(labyrinth, column + 1))
+	end_point.append(apply_graphical_offset(labyrinth, row + 1) - labyrinth.cell_size/2)
+
+	ligne(start_point[0], start_point[1], end_point[0], end_point[1], epaisseur=2)
+
+def draw_invisible_comma(labyrinth, row, column) :
+	x = (column / 2 * labyrinth.window_size / labyrinth.cell_number) + labyrinth.margin
+	y = (row / 2 * labyrinth.window_size / labyrinth.cell_number) + labyrinth.margin
+	point(x, y, epaisseur=0)
+
+def draw_picture(labyrinth, row, column, pathToFile) :
+	x = (column / 2 * labyrinth.window_size / labyrinth.cell_number) + labyrinth.margin
+	y = (row / 2 * labyrinth.window_size / labyrinth.cell_number) + labyrinth.margin
+	image(x, y, pathToFile)
+
+picturePathDictionnary = {
+	'A' : "media/ariane.png",
+	'T' : "media/thesee.png",
+	'V' : "media/minoV.png",
+	'H' : "media/minoH.png",
+	'P' : "media/porte.png"
+}
+
+functionDictionnary = {
+	'+' : lambda l,r,c : draw_invisible_comma(l,r,c),
+	'\n' : lambda l,r,c : draw_invisible_comma(l,r,c),
+	' ' : lambda l,r,c : draw_invisible_comma(l,r,c),
+	'|' : lambda l,r,c : draw_vertical_wall(l,r,c),
+	'-' : lambda l,r,c : draw_horizontal_wall(l,r,c),
+	'A' : lambda l,r,c : draw_picture (l,r,c,picturePathDictionnary['A']),
+	'T' : lambda l,r,c : draw_picture (l,r,c,picturePathDictionnary['T']),
+	'V' : lambda l,r,c : draw_picture (l,r,c,picturePathDictionnary['V']),
+	'H' : lambda l,r,c : draw_picture (l,r,c,picturePathDictionnary['H']),
+	'P' : lambda l,r,c : draw_picture (l,r,c,picturePathDictionnary['P']),
+}
 
 display_map(lab.lst)
-draw_map(lab, 800, 800)
+draw_map(lab, 810, 810)
 # define_assets(lab.lst)
 """
 	10 points jeu

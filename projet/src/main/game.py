063dc73119852f31aed99662ca8dd352167305db
Cloudy <cloud.kira@gmail.com>
2020-04-17 21:48:07 +0200

    move minotaurs only if player could move

--- projet/src/main/game.py
+++ projet/src/main/game.py
@@ -1,21 +1,28 @@
 from utils.graphics import *
 from utils.labyrinth_utils import *
+import sys
 
 
 def game():
-	lab = parse_map("../../maps/sandbox.txt")
+	if len(sys.argv) != 2:
+		print("Usage: python game.py path_to_file\n")
+		return
+
+	path = "../../maps/" + sys.argv[1]
+
+	lab = parse_map(path)
 	w_size = 810
 	cree_fenetre(w_size + lab.margin, w_size + lab.margin)
 
 	while True:
+		if lab.player.exit_reached and lab.player.has_thesee:
+			image(w_size/2, w_size/2, picturePathDictionary['E'])
+			attend_clic_gauche()
+			break
 
 		efface_tout()
 		draw_map(lab)
 
-		if lab.player.has_thesee and lab.player.exit_reached:
-			image(w_size/2, w_size/2, picturePathDictionary['E'])
-			attend_clic_gauche()
-			break
 		if lab.player.has_minotaur:
 			image(w_size/2, w_size/2, picturePathDictionary['D'])
 			attend_clic_gauche()
@@ -24,23 +31,25 @@ def game():
 		event = attend_ev()
 		event_type = type_ev(event)
 
+		valid_move = False
+
 		if event_type == 'Touche':
 			key_name = touche(event)
 			if key_name == 'Left':
-				lab.compute_left()
+				valid_move = lab.compute_left()
 			elif key_name == 'Right':
-				lab.compute_right()
+				valid_move = lab.compute_right()
 			elif key_name == 'Up':
-				lab.compute_up()
+				valid_move = lab.compute_up()
 			elif key_name == 'Down':
-				lab.compute_down()
+				valid_move = lab.compute_down()
 			elif key_name == 'q':
 				break
 			else:
 				pass
 
-		lab.move_minotaurs()
-		mise_a_jour()
+		if valid_move:
+			lab.move_minotaurs()
 
 
 game()
